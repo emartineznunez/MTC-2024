1c6b3c26687b19eff987befc8f189fb6fa57a8f6
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2022-04-05 12:13:33 +0200

    Add files via upload

---
+++ launch_hl.sh
@@ -0,0 +1,4 @@
+module load gcccore/system automekin/2021 
+module load g09
+sbatch -n 8 -c 4 -t 00:20:00 hlcalcs.sh FA.dat 
+
---
+++ launch_ll.sh
@@ -0,0 +1,3 @@
+module load gcccore/system automekin/2021 
+sbatch -n 16 -t 00:10:00 llcalcs.sh FA.dat 10 2
+
